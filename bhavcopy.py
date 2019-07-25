3e05b6b6cff2c1a055ecf47311c16fd66c62f1bc
Rushil Singh <rushilsingh@gmail.com>
2019-07-25 17:16:08 +0530

    Download latest bhavcopy

--- bhavcopy.py
+++ bhavcopy.py
@@ -1,4 +1,29 @@
+import os
 import requests
+import io
+import zipfile
+import datetime
 
-response = requests.get("https://www.bseindia.com/download/BhavCopy/Equity/EQ250719_CSV.ZIP")
-print(dir(response))
+class BhavCopy(object):
+    
+    def __init__(self):
+        current = str(datetime.datetime.now())
+        current = current.split()[0]
+        current = current.split("-")
+        day = current[2]
+        month = current[1]
+        year = current [0][2:]
+        today = day+month+year
+        url = "https://www.bseindia.com/download/BhavCopy/Equity/EQ"+today+"_CSV.ZIP"
+        response = requests.get(url, stream=True)
+        z = zipfile.ZipFile(io.BytesIO(response.content))
+        z.extractall()
+        with open("EQ"+today+".CSV") as f:
+            text = f.read()
+            
+        os.unlink("EQ"+today+".CSV")
+        print text
+
+
+bhavcopy = BhavCopy()
+
