cd5bed42916afc66d2576d8dd4a6ec8571437cfc
Rushil Singh <rushilsingh@gmail.com>
2019-07-26 15:04:54 +0530

    Redis

--- bhavcopy.py
+++ bhavcopy.py
@@ -5,10 +5,10 @@ import zipfile
 import datetime
 
 class BhavCopy(object):
-    
+
     def __init__(self):
         self.base_url = "https://www.bseindia.com/download/BhavCopy/Equity/EQ{}_CSV.ZIP"
-        self.base_fname = "EQ{}.CSV" 
+        self.base_fname = "EQ{}.CSV"
         response = None
         timedelta = 0
         while(response is None or response.status_code != 200):
@@ -24,22 +24,44 @@ class BhavCopy(object):
         z.extractall()
         with open(self.fname) as f:
             self.text = f.read()
-            
-        os.unlink(self.fname)
-        
+
+        print self.fname
+        #os.unlink(self.fname)
+
 
     def get_date_string(self, timedelta=0):
-        
+
         now = datetime.datetime.today() - datetime.timedelta(days=timedelta)
         day = str(now.day) if len(str(now.day))==2 else (str(0) + str(now.day))
         month = str(now.month) if len(str(now.month))==2 else (str(0) + str(now.month))
         year = str(now.year)[-2:]
         year = year if len(year)==2 else (str(0)+year)
         return (day+month+year)
-    
+
     def get_url(self):
         return self.base_url.format(self.date_string)
 
-bhavcopy = BhavCopy()
-print(bhavcopy.text)
+    def parse(self):
+        self.headers = self.text.split()[0].split(",")
+        base_command = """ awk -F, 'BEGIN {OFS=","} %s' """ +  self.fname + """ | sed 's/\\"//g' """
+        inner_command = "{ print \"SET\"%s}"
+        columns = ""
+        for i in range(len(self.headers)):
+            columns += ",$%s " % (i+1)
 
+        inner_command = inner_command % columns
+        command = base_command % inner_command
+        os.system(command)
+
+
+        with open(self.fname) as f:
+            self.text = f.read()
+        os.unlink(self.fname)
+        self.fname = "output.txt"
+        with open(self.fname, "w") as f:
+            f.write(self.text)
+        os.system("cat %s | python gen_redis_proto.py | redis-cli --pipe" % self.fname)
+
+
+
+BhavCopy().parse()
