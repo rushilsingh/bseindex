5209d2437f022cbe278ecb6709a426e002341a40
Rushil Singh <rushilsingh@gmail.com>
2019-07-26 16:25:33 +0530

    Configuring cherrypy

--- app.py
+++ app.py
@@ -3,6 +3,17 @@ from bhavcopy import BhavCopy
 import cherrys
 cherrypy.lib.sessions.RedisSession = cherrys.RedisSession
 
+config = {
+    'tools.sessions.on' : True,
+    'tools.sessions.storage_type' : 'redis',
+    'tools.sessions.host' : '127.0.0.1',
+    'tools.sessions.port' : 6379,
+    'tools.sessions.db' : 0,
+    'tools.sessions.password' : None
+    }
+cherrypy.config.update(config)
+
+
 class App(object):
     @cherrypy.expose
     def index(self):
