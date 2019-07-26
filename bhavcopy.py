9ee444f055bc816324463b5955591297aed9ca24
Rushil Singh <rushilsingh@gmail.com>
2019-07-26 16:00:03 +0530

    Configuring redis commands

--- bhavcopy.py
+++ bhavcopy.py
@@ -9,31 +9,33 @@ class BhavCopy(object):
     def __init__(self):
         self.base_url = "https://www.bseindia.com/download/BhavCopy/Equity/EQ{}_CSV.ZIP"
         self.base_fname = "EQ{}.CSV"
-        response = None
-        timedelta = 0
-        while(response is None or response.status_code != 200):
-            self.date_string = self.get_date_string(timedelta)
-            self.url = self.base_url.format(self.date_string)
-            print(self.url)
-            response = requests.get(self.url, stream=True)
-            print(response.status_code)
+        self.response = None
+        self.timedelta = 0
 
-            timedelta += 1
+    def download(self):
+
+        while(self.response is None or self.response.status_code != 200):
+            self.date_string = self.get_date_string()
+            self.url = self.base_url.format(self.date_string)
+            self.response = requests.get(self.url, stream=True)
+
+            self.timedelta += 1
+        self.timedelta = 0
+
         self.fname = self.base_fname.format(self.date_string)
-        z = zipfile.ZipFile(io.BytesIO(response.content))
+        z = zipfile.ZipFile(io.BytesIO(self.response.content))
         z.extractall()
         with open(self.fname) as f:
             self.text = f.read()
 
-        #os.unlink(self.fname)
 
 
-    def get_date_string(self, timedelta=0):
+    def get_date_string(self):
 
-        now = datetime.datetime.today() - datetime.timedelta(days=timedelta)
-        day = str(now.day) if len(str(now.day))==2 else (str(0) + str(now.day))
-        month = str(now.month) if len(str(now.month))==2 else (str(0) + str(now.month))
-        year = str(now.year)[-2:]
+        stamp = datetime.datetime.today() - datetime.timedelta(days=self.timedelta)
+        day = str(stamp.day) if len(str(stamp.day))==2 else (str(0) + str(stamp.day))
+        month = str(stamp.month) if len(str(stamp.month))==2 else (str(0) + str(stamp.month))
+        year = str(stamp.year)[-2:]
         year = year if len(year)==2 else (str(0)+year)
         return (day+month+year)
 
@@ -48,29 +50,29 @@ class BhavCopy(object):
         for i in range(len(self.headers)):
             columns += ",$%s" % (i+1)
 
-
-        self.fname = "output.txt"
         inner_command = inner_command % columns
         command = base_command % inner_command
-        print(command)
         os.system(command)
 
+        os.unlink(self.fname)
+        self.fname = "output.txt"
         with open(self.fname) as f:
             self.text = f.readlines()
-        
+
         parsed_commands = ""
         for line in self.text:
             line = line.rstrip().rstrip(",")
             line = " ".join(line.split(","))
             parsed_commands += (line) + "\n"
-        print(parsed_commands)
         os.unlink(self.fname)
         with open(self.fname, "w") as f:
             f.write(parsed_commands)
 
+        os.system("cat %s; sleep 5 | redis-cli --pipe" % self.fname)
+        os.unlink(self.fname)
 
-        os.system("cat %s | redis-cli --pipe" % self.fname)
 
-
-
-BhavCopy().parse()
+if __name__ == "__main__":
+    bhavcopy = BhavCopy()
+    bhavcopy.download()
+    bhavcopy.parse()
