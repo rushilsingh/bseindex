0481e6cfefa6e451627767bd4623fb3e306cd448
Rushil Singh <rushilsingh@gmail.com>
2019-07-30 21:22:42 +0530

    Sorting top ten stocks

--- app.py
+++ app.py
@@ -19,26 +19,41 @@ class App(object):
     @cherrypy.expose
     def index(self):
         red = redis.Redis("localhost", 6379)
-        output = ""
+        output = []
+        dictionary = {}
         index = 1
+        max = None
         while True:
-            keys = red.keys('*[A-Za-z]%s' % (index))
-            vals = red.mget(keys)
-            if len(vals) == 0:
+            keys = red.keys('*Change%s' % (index))
+            if len(keys) == 0:
                 break
-            kv = zip(keys, vals)
-            for pair in kv:
-                output += pair[0] + " : " +pair[1] + " , "
-            output += "<break />"
-            open_index = red.keys('Open%s' % index)
-            open_index = red.mget(open_index[0])[0]
-            close_index = red.keys('Close%s' % index)
-            close_index = red.mget(close_index[0])[0]
-            open_index = float(open_index)
-            diff = float(open_index) - float(close_index)
-
+            key = keys[0]
+            value = red.mget(key)[0]
+            output.append(value)
+            dictionary[index] = value
             index += 1
+        output.sort()
+        output.reverse()
+        output = output[0:10]
+        results = []
+        index = 0
+        for value in output:
+            for key in dictionary:
+                if dictionary[key] == value:
+                    results.append(key)
+                    break
+        output = ""
+        for index in results:
+            keys = red.keys("*[A-Za-z]%s" % index)
+            values = red.mget(keys)
+            del_string = len(str(index))
+            for i in range(len(keys)):
+                output += keys[i][:-del_string] + " : " + values[i] + " , "
+            output += "<br />"
         return output
+
+
+
 
 
 
