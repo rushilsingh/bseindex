862d5c6ee276a8a8344f18ca991ab6d95b337ad3
Rushil Singh <rushilsingh@gmail.com>
2019-07-30 20:09:43 +0530

    Building frontend

--- app.py
+++ app.py
@@ -19,19 +19,29 @@ class App(object):
     @cherrypy.expose
     def index(self):
         red = redis.Redis("localhost", 6379)
-        keys = red.keys('*')
-        vals = []
-        for key in keys:
-            type = red.type(key)
-            if type == KV:
-                val = red.get(key)
-            if type == HASH:
-                vals = red.hgetall(key)
-            if type == ZSET:
-                 vals = red.zrange(key, 0, -1)
-        kv = zip(keys, vals)
-        print kv
-        return kv
+        output = ""
+        index = 1
+        while True:
+            keys = red.keys('*[A-Za-z]%s' % (index))
+            vals = red.mget(keys)
+            if len(vals) == 0:
+                break
+            kv = zip(keys, vals)
+            for pair in kv:
+                output += pair[0] + " : " +pair[1] + " , "
+            output += "<break />"
+            open_index = red.keys('Open%s' % index)
+            open_index = red.mget(open_index[0])[0]
+            close_index = red.keys('Close%s' % index)
+            close_index = red.mget(close_index[0])[0]
+            open_index = float(open_index)
+            diff = float(open_index) - float(close_index)
+
+            index += 1
+        return output
+
+
+
 
 if __name__ == '__main__':
     bhavcopy = BhavCopy()
